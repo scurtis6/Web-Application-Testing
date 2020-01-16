695b4259e46d290963592c4ab60c225a6bf97146
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-16 16:49:48 -0500

    set up functions for when balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -4,6 +4,18 @@ const Dashboard = () => {
     const [ball, setBall] = useState(0);
     const [strike, setStrike] = useState(0);
     const [foul, setFoul] = useState(0);
+
+    if (strike >= 3) {
+        setBall(0);
+        setStrike(0);
+    }
+
+    if (ball >= 4) {
+        setBall(0);
+        setStrike(0);
+    }
+
+    
 }
 
 export default Dashboard;
