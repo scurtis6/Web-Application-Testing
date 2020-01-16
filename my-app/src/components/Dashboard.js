a6b4a7df22587e16c3aded229799840cf4e3bbfb
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-16 16:45:19 -0500

    set up useState

---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Dashboard = () => {
+    const [ball, setBall] = useState(0);
+    const [strike, setStrike] = useState(0);
+    const [foul, setFoul] = useState(0);
+}
+
+export default Dashboard;
