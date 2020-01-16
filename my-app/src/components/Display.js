a371704bf035fcb2ecfcecf27518ae902f863105
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-16 18:39:21 -0500

    adding style

--- my-app/src/components/Display.js
+++ my-app/src/components/Display.js
@@ -4,7 +4,7 @@ const Display = props => {
     console.log(props);
     return (
         <div>
-            <h2>Display</h2>
+            {/* <h2>Display</h2> */}
             <p>Strikes: {props.strike}</p>
             <p>Balls: {props.ball}</p>
             {/* <p>Hits: {props.hit}</p> */}
