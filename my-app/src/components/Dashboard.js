3c1b446ad4aac05e207f5cadd9b0c0dd34c72083
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-16 18:18:25 -0500

    finished adding runs and errors in the inning stretch goal

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Display from './Display';
 import Inning from './Inning';
+import RunsandErrors from './RunsandErrors';
 
 const Dashboard = () => {
     const [ball, setBall] = useState(0);
@@ -8,6 +9,8 @@ const Dashboard = () => {
     const [foul, setFoul] = useState(0);
     const [inning, setInning] = useState(1);
     const [out, setOut] = useState(0);
+    const [run, setRun] = useState(0);
+    const [error, setError] = useState(0);
 
     if (strike >= 3) {
         setBall(0);
@@ -24,7 +27,7 @@ const Dashboard = () => {
     }
 
     if (out >= 3){
-        setOut(0)
+        setOut(0);
     }
 
     const hit = () => {
@@ -61,6 +64,14 @@ const Dashboard = () => {
         setOut(out +1)
     }
 
+    const addRun = () => {
+        setRun(run + 1)
+    }
+
+    const addError = () => {
+        setError(error + 1)
+    }
+
     return (
         <div>
            <div className='display'>
@@ -69,11 +80,16 @@ const Dashboard = () => {
             <button onClick={addBall}>Ball</button>
             <button onClick={foulBall}>Foul</button>
             <button onClick={hit}>Hit</button>
+            </div>
+            <div className="inning">
+                <Inning inning={inning} out={out} />
+                <button onClick={addInning}>Inning</button>
+                <button onClick={addOut}>Out</button>
            </div>
-           <div className="inning">
-               <Inning inning={inning} out={out} />
-               <button onClick={addInning}>Inning</button>
-               <button onClick={addOut}>Out</button>
+           <div className="run-error">
+               <RunsandErrors run={run} error={error} />
+               <button onClick={addRun}>Run</button>
+               <button onClick={addError}>Error</button>
            </div>
         </div>
     )
