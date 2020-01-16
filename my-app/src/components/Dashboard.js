74e9fc51d75c193e4e831568430842609ab05907
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-16 17:44:08 -0500

    finished mvp goals

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Display from './Display';
+import Inning from './Inning';
 
 const Dashboard = () => {
     const [ball, setBall] = useState(0);
@@ -15,7 +17,41 @@ const Dashboard = () => {
         setStrike(0);
     }
 
-    
+    const hit = () => {
+        setBall(0);
+        setStrike(0);
+    }
+
+    const foulBall = () => {
+        if (strike === 0) {
+            setFoul(1);
+            setStrike(1);
+        } else if (strike === 1) {
+            setFoul(2);
+            setStrike(2);
+        } else if (strike === 2) {
+            setFoul(foul + 1);
+            setStrike(2);
+        }
+    };
+
+    const addStrike = () => {
+        setStrike(strike + 1);
+    }
+
+    const addBall = () => {
+        setBall(ball + 1);
+    }
+
+    return (
+        <div>
+            <Display ball={ball} strike={strike} hit={hit} />
+            <button onClick={addStrike}>Strike</button>
+            <button onClick={addBall}>Ball</button>
+            <button onClick={foulBall}>Foul</button>
+            <button onClick={hit}>Hit</button>
+        </div>
+    )
 }
 
 export default Dashboard;
---
+++ my-app/src/components/Inning.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inning = props => {
+    return (
+        <div>
+            <p>Innings:</p>
+            <p>Outs:</p>
+        </div>
+    )
+}
+
+export default Inning;
